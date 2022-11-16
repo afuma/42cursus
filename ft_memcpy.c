7813f02336f5df0ffe8a2e95efa2a795e497da20
Etienne DESAINTJEAN <edesaint@e2r7p11.42.fr>
2022-11-16 19:33:47 +0100

    first push

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: edesaint <edesaint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 17:49:31 by edesaint          #+#    #+#             */
-/*   Updated: 2022/11/16 19:08:00 by edesaint         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:13:55 by edesaint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,10 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     unsigned char *d;
     size_t i;
 
-    s_dest = dest;
     s = (unsigned char *) src;
     d = (unsigned char *) dest;
     i = 0;
-    // je crois qu'il n'y a pas de condition sur le \0
-    // puisque c'est n bytes (et pas NULL-terminated dans la description)
-    while(s[i] != '\0' && i <= n)
+    while(i <= n)
     {
         d[i] = s[i];
         i++;
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,31 +6,26 @@
 /*   By: edesaint <edesaint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 17:50:08 by edesaint          #+#    #+#             */
-/*   Updated: 2022/11/15 19:23:22 by edesaint         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:32:45 by edesaint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <bsd/string.h>
 #include "libft.h"
+// utiliser le flags de compilation -lbsd
 
 size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
-    int i;
+    size_t len_src;
 
-    i = 0;
-    if (!src)
-    	return (NULL);
-    if (*src == '\0')
-        return (0);
-    if (size <= 0)
-        return (strlen(src));
-    while(*src != '\0' && i <= size - 1)
-    {
-        *dst = *src;
-        src++;
-        dst++;
-        i++;
-    }
-    *dst = '\0';
+    len_src = ft_strlen(src);
+    if (!src || !*src)
+    	return (0);
+    if (size > len_src)
+        ft_memcpy(dst, src, size);
 
-    return (strlen(src)); //return (i);
+    return (len_src);
 }
+
+// (pas\0, len_src=3, size=3, on ne peut pas copie le \0, on entre)
+// (pas\0, len_src=3, size=4, on peut copier le \0, on entre pas)
